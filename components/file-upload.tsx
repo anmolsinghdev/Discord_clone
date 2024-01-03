e948a1a3da80ef2490207840b59d04b6752ac487
Anmol Singh <anmolsinghdev@gmail.com>
2024-01-03 12:57:26 +0530

    adding uploadthing and create image upload component

---
+++ components/file-upload.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { UploadDropzone } from "@/lib/uploadthing";
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image src={value} fill className="rounded-full" alt="Image" />
+        <button
+          onChange={() => onChange("")}
+          className="bg-rose-500 p-1 rounded-full absolute top-0 right-0 shadow-sm "
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <UploadDropzone
+        endpoint={endpoint}
+        onClientUploadComplete={(res) => {
+          onChange(res?.[0].url);
+        }}
+        onUploadError={(error: Error) => {
+          console.log("error", error);
+        }}
+      />
+    </div>
+  );
+};
